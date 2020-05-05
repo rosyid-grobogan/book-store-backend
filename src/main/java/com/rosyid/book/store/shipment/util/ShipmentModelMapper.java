3a5eb907c07ce632d2968c8221dbad87e8291095
mas rosyid <rosyidgrobogan@gmail.com>
2020-05-05 10:23:36 +0700

    add ShipmentModelMapper

---
+++ src/main/java/com/rosyid/book/store/shipment/util/ShipmentModelMapper.java
@@ -0,0 +1,37 @@
+package com.rosyid.book.store.shipment.util;
+
+
+import com.rosyid.book.store.account.payload.response.UserResponse;
+import com.rosyid.book.store.shipment.entity.Shipment;
+import com.rosyid.book.store.shipment.payload.response.ShipmentResponse;
+import com.rosyid.book.store.transaction.util.TransactionModelMapper;
+import org.springframework.beans.BeanUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShipmentModelMapper
+{
+    public static ShipmentResponse constructModel(Shipment shipment)
+    {
+        UserResponse userModel = new UserResponse();
+        BeanUtils.copyProperties(shipment.getUser(), userModel);
+
+        ShipmentResponse model = new ShipmentResponse();
+        model.setUserResponse(userModel);
+        model.setTransactionResponse(TransactionModelMapper.constructModel(shipment.getTransaction()));
+
+        BeanUtils.copyProperties(shipment, model);
+        return model;
+    }
+
+    public static List<ShipmentResponse> constructModel(List<Shipment> shipments)
+    {
+        List<ShipmentResponse> models = new ArrayList<>();
+        shipments.forEach(shipment -> {
+            ShipmentResponse model = constructModel(shipment);
+            models.add(model);
+        });
+        return models;
+    }
+}
