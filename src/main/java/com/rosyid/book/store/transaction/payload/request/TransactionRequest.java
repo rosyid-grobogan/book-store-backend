af3d99a281c1ed20955b4f257c401ec82fdce735
mas rosyid <rosyidgrobogan@gmail.com>
2020-05-05 08:25:46 +0700

    add TransactionRequest

---
+++ src/main/java/com/rosyid/book/store/transaction/payload/request/TransactionRequest.java
@@ -0,0 +1,18 @@
+package com.rosyid.book.store.transaction.payload.request;
+
+import javax.validation.constraints.NotNull;
+import java.util.Set;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TransactionRequest
+{
+
+    @NotNull
+    private Long userId;
+
+    @NotNull
+    private Set<Long> cartDetailIds;
+}
