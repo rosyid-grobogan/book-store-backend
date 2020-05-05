8d30bfc80c2cbf7a519ede16caebee0d6bab59cf
mas rosyid <rosyidgrobogan@gmail.com>
2020-05-05 10:14:14 +0700

    modify shipment response

--- src/main/java/com/rosyid/book/store/shipment/payload/response/ShipmentResponse.java
+++ src/main/java/com/rosyid/book/store/shipment/payload/response/ShipmentResponse.java
@@ -1,12 +1,20 @@
 package com.rosyid.book.store.shipment.payload.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.rosyid.book.store.account.payload.response.UserResponse;
 import com.rosyid.book.store.shipment.entity.EnumCourier;
 import com.rosyid.book.store.shipment.entity.EnumShipmentStatus;
 import com.rosyid.book.store.shipment.persistence.ShipmentModelPersistence;
 import com.rosyid.book.store.transaction.payload.response.TransactionResponse;
+import lombok.Getter;
+import lombok.Setter;
 
-
+/**
+ * DTO
+ */
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ShipmentResponse extends ShipmentModelPersistence
 {
 
